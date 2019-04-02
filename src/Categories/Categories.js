b76ae7f41f1ae0f7c6e19b3caebdac7bdc84f6ae
wenhuiliu <wenhuiliu.whl@gmail.com>
2019-04-01 19:16:56 -0700

    update category

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import style from './Catrgories.module.css';
 
 const Categories = ({ categories }) => (
-  <ul>
+  <ul className={style.categories}>
     {
       categories.map(category => {
         return (
           <li key={category.id}>
-            <Link to={`/products/${category.id}`}>
-              {category.name}
+            <Link to={`/products/${category.id}`} className={style.link}>
+              <div className={style.category}>
+                <img src={category.img.sm} alt = {category.name}/>
+                <span>{category.name}</span>
+              </div>
             </Link>
           </li>
         );
